aaf7509f307cbc5bdd151771dc2159383d5a775f
anupkokkalki <anupkokkalki@gmail.com>
2025-08-25 17:19:26 +0530

    working code

--- src/Pages/Bye.java
+++ src/Pages/Bye.java
@@ -5,13 +5,7 @@ public class Bye {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		tested();
-		mastered();
-
-			
-		featured();
-added this line on new feature branch();
-mastered();
+		System.out.println("Working Fine");
 
 	}
 
